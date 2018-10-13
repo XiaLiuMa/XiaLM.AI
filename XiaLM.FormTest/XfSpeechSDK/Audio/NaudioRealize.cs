3888a5c7a0a80f792c147b48023b23fcbe89afcd
XiaLiuMa <am6x45@126.com>
2018-10-13 22:19:36 +0800

    1

---
+++ XiaLM.FormTest/XfSpeechSDK/Audio/NaudioRealize.cs
@@ -0,0 +1,15 @@
+﻿using NAudio.Wave;
+
+namespace XiaLM.FormTest.XfSpeechSDK.Audio
+{
+    public class NaudioRealize
+    {
+        private WaveCallbackInfo wavInCallBack;
+
+        public void Test()
+        {
+            WaveIn waveIn = new WaveIn(WaveCallbackInfo.FunctionCallback());
+            IWaveIn waveIn1 = new WaveInEvent();
+        }
+    }
+}
